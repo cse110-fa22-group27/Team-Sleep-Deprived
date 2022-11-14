608f664e29c2253483d0a4e4f890fd8fc83c3886
achen200 <54335096+achen200@users.noreply.github.com>
2022-11-14 12:38:40 -0800

    Ran lint on existing files, fixing to meet syntax

--- source/javascript/WalletInfo.js
+++ source/javascript/WalletInfo.js
@@ -1,43 +1,43 @@
 class WalletInfo extends HTMLElement {
-    constructor() {
-        super();
-        this.shadowElem = this.attachShadow({mode: "open"});
-        this.elementRoot = document.createElement('div');
-        this.elementRoot.className = 'wallet-info glass-box';
+	constructor() {
+		super();
+		this.shadowElem = this.attachShadow({mode: 'open'});
+		this.elementRoot = document.createElement('div');
+		this.elementRoot.className = 'wallet-info glass-box';
 
-        this.walletName = document.createElement('h3');
-        this.walletName.className = 'wallet-name';
+		this.walletName = document.createElement('h3');
+		this.walletName.className = 'wallet-name';
 
-        this.walletAmount = document.createElement('p');
-        this.walletAmount.className = 'wallet-amount';
+		this.walletAmount = document.createElement('p');
+		this.walletAmount.className = 'wallet-amount';
 
-        this.walletLastTransaction = document.createElement('p');
-        this.walletLastTransaction.className = 'wallet-last-transaction';
-        this.walletLastTransaction.dataset.transactionType = "negative";
+		this.walletLastTransaction = document.createElement('p');
+		this.walletLastTransaction.className = 'wallet-last-transaction';
+		this.walletLastTransaction.dataset.transactionType = 'negative';
 
-        this.styleElem = document.createElement('link');
-        this.styleElem['rel'] = 'stylesheet';
-        this.styleElem['href'] = '../css/wallet-styles.css';
+		this.styleElem = document.createElement('link');
+		this.styleElem['rel'] = 'stylesheet';
+		this.styleElem['href'] = '../css/wallet-styles.css';
 
-        this.elementRoot.append(this.walletName, this.walletAmount, this.walletLastTransaction, this.styleElem);
-        this.shadowElem.append(this.elementRoot);
-    }
+		this.elementRoot.append(this.walletName, this.walletAmount, this.walletLastTransaction, this.styleElem);
+		this.shadowElem.append(this.elementRoot);
+	}
 
-    /**
+	/**
      * data: {
      *   name: String,
      *   amount: Number,
      *   lastTransaction: {name: String, amount: Number, positive: bool}
      * }
     **/
-    set data(data) {
-        console.log(this.walletAmount);
-        this.walletName.innerHTML = data.name;
-        this.walletAmount.innerHTML = `$${data.amount}`;
-        this.walletLastTransaction.innerHTML = `<strong>${data.lastTransaction.name}</strong> $${data.lastTransaction.amount}`;
-        console.log(data.lastTransaction.type);
-        this.walletLastTransaction.dataset.transactionType = data.lastTransaction.type;
-    }
+	set data(data) {
+		console.log(this.walletAmount);
+		this.walletName.innerHTML = data.name;
+		this.walletAmount.innerHTML = `$${data.amount}`;
+		this.walletLastTransaction.innerHTML = `<strong>${data.lastTransaction.name}</strong> $${data.lastTransaction.amount}`;
+		console.log(data.lastTransaction.type);
+		this.walletLastTransaction.dataset.transactionType = data.lastTransaction.type;
+	}
 
 
 }
