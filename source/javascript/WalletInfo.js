c96f071bbc889f3f72b015bbbd53376ef07ca244
Ashwin Rohit <aalagiri@ucsd.edu>
2022-11-13 20:38:13 -0800

    Modify gitignore to add the docs

--- source/javascript/WalletInfo.js
+++ source/javascript/WalletInfo.js
@@ -1,25 +1,56 @@
+/**
+ * @author Ashwin Rohit Alagiri Rajan
+ * @class
+ * @implements {HTMLElement}
+ * WalletInfo custom component. This contains the custom component definition for wallet-info.
+ */
 class WalletInfo extends HTMLElement {
+    /** 
+     * @constructs WalletInfo
+     */
     constructor() {
         super();
         this.shadowElem = this.attachShadow({mode: "open"});
+        
+        /**
+         * The root div of the wallet-info component
+         * @member {HTMLElement} elementRoot 
+         */
         this.elementRoot = document.createElement('div');
         this.elementRoot.className = 'wallet-info glass-box';
 
+        /**
+         * The title of the wallet-info component
+         * @member {HTMLElement} walletName
+         */
         this.walletName = document.createElement('h3');
         this.walletName.className = 'wallet-name';
 
+        /**
+         * Displays the wallet amount of the component
+         * @member {HTMLElement} walletAmount
+         */
         this.walletAmount = document.createElement('p');
         this.walletAmount.className = 'wallet-amount';
 
+        /**
+         * Displays the last transaction with the amount. Colors it according to the type of transaction.
+         * @member {HTMLElement} walletLastTransaction
+         */
         this.walletLastTransaction = document.createElement('p');
         this.walletLastTransaction.className = 'wallet-last-transaction';
         this.walletLastTransaction.dataset.transactionType = "negative";
 
+        /**
+         * Styles the wallet info component. The stylesheet is defaulted to <code>../css/wallet-styles.css</code>
+         * @member {HTMLElement} styleElem
+         */
         this.styleElem = document.createElement('link');
         this.styleElem['rel'] = 'stylesheet';
         this.styleElem['href'] = '../css/wallet-styles.css';
 
         this.elementRoot.addEventListener('click', event => {
+            event.preventDefault();
             window.open('../../source/html/wallet_info.html', '_self');
         });
 
@@ -28,19 +59,28 @@ class WalletInfo extends HTMLElement {
     }
 
     /**
-     * data: {
-     *   name: String,
-     *   amount: Number,
-     *   lastTransaction: {name: String, amount: Number, positive: bool}
-     * }
-    **/
+     * @typedef {Object} transaction
+     * @property {string} name Name of the last transaction
+     * @property {number} amount Last transaction amount
+     * @property {string} type Last transaction type - either "positive" or "negative" 
+     */
+
+    /**
+     * @typedef {Object} data
+     * @property {string} name Name of the wallet
+     * @property {number} amount The amount remaining in the wallet
+     * @property {transaction} lastTransaction Last transaction details
+     */
+
+    /**
+    * @param {data} data The data object that contains the wallet information
+    */
     set data(data) {
         this.walletName.innerHTML = data.name;
         this.walletAmount.innerHTML = `$${data.amount}`;
         this.walletLastTransaction.innerHTML = `<strong>${data.lastTransaction.name}</strong> $${data.lastTransaction.amount}`;
         this.walletLastTransaction.dataset.transactionType = data.lastTransaction.type;
     }
-
 
 }
 customElements.define('wallet-info', WalletInfo);
