2bed468502a0ad99226f24cdbdd38d4442560900
Jacob Graven <jacobmgraven@gmail.com>
2022-11-15 13:02:26 -0800

    Update source/javascript/Report.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- source/javascript/Report.js
+++ source/javascript/Report.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 });
 
 window.addEventListener('load', () => {
-	let xLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let xLabels = ['Jan', "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	// Sample Values - TODO: loop over transactions from each month, assign to yValues
 	let yValues = [2500, 3500, 3000, 2100, 1000, 1500, 1250, 500, 750, 900, 1000, 1500];
 	// let yMax = Math.max.apply(null, yValues);
