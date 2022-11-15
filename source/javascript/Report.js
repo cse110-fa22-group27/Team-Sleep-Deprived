49a61c6c02007736e07eef0a2058438e6ba22dc2
jacobgraven <jacobmgraven@gmail.com>
2022-11-14 17:43:57 -0800

    report page finishing styling

--- source/javascript/Report.js
+++ source/javascript/Report.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => {
 
 window.addEventListener('load', () => {
   let xLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  // Sample Values - TODO: loop over transactions from each month, assign to yValues
+  // Sample Values - TODO: loop over transactions from each month, assign proper values to yValues
   let yValues = [2500, 3500, 3000, 2100, 1000, 1500, 1250, 500, 750, 900, 1000, 1500];
 
   new Chart("report-chart", {
