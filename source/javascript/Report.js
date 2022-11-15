88c155de4a3d3e2a8696c4617467c876c03e4219
Jacob Graven <jacobmgraven@gmail.com>
2022-11-15 13:02:49 -0800

    Update source/javascript/Report.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- source/javascript/Report.js
+++ source/javascript/Report.js
@@ -10,7 +10,7 @@ window.addEventListener('load', () => {
 	let yValues = [2500, 3500, 3000, 2100, 1000, 1500, 1250, 500, 750, 900, 1000, 1500];
 	// let yMax = Math.max.apply(null, yValues);
 
-	new Chart("report-chart", {
+  new Chart('report-chart', {
     type: 'bar',
 		data: {
 			labels: xLabels,
