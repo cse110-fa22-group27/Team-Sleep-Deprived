0a06d3db8042c7f25afcefa53b6441777e3aa35f
kc092444 <kc092444@gmail.com>
2022-11-14 17:40:58 -0800

    dashboard layout/add-tran

--- source/javascript/transaction.js
+++ source/javascript/transaction.js
@@ -1,16 +1,40 @@
 class TransactionElement extends HTMLElement {
     constructor() {
         super();
-        const shadow = this.attachShadow({ mode: "open" });
-        // const root = shadow.shadowRoot;
-        // var trans = document.createElement("div");
-        // var name = document.createElement("p");
-        // var amount = document.createElement("p");
-        // var date = document.createElement("p");
+        this.attachShadow({ mode: "open" });
+        const divider = document.createElement('div');
+        this.shadowRoot.appendChild(divider);
 
-        // var style = document.createElement("style");
-        // style.textContent = {};
+    }
 
+    /**
+     * Called when the .data property is set on this element.
+     * 
+     * For Example:
+     * let transaction = document.createElement('trans-elem'); // Calls constructor()
+     * transaction.data = { foo: 'bar' } // Calls set data({ foo: 'bar' })
+     *
+     * @param {Object} data - The data to pass into the <recipe-card>, must be of the
+     *                        following format:
+     *                        {
+     *                          "name": "string",
+     *                          "amount": "double",
+     *                          "wallet": "string"
+     *                        }
+     */
+    set data(data) {
+        // if data null return
+        if(!data) {
+            return;
+        }
+        var transaction = this.shadowRoot.querySelector('div');
+        transaction.innerHTML = `
+        <div class="flex-container">
+            <p id="name">${data.name}</p>
+            <p id="amount">(${data.amount})</p>
+            <p id="wallet">${data.wallet}</p>
+        </div>
+        `
     }
 }
 customElements.define("trans-elem", TransactionElement);
