a3a0dc2c2b121b22bc38acc2e4e6078d41c03364
Ashwin Rohit <aalagiri@ucsd.edu>
2022-11-14 20:09:25 -0800

    Basic Changes to layout

--- source/javascript/Wallet.js
+++ source/javascript/Wallet.js
@@ -1,9 +1,9 @@
 const walletGrid = document.querySelector('#wallets-grid');
 const localStorageString = window.localStorage.getItem('wallet-infos');
 const localStorageWallets = JSON.parse(localStorageString != null?localStorageString:'[]');
+
 for(let wallet of localStorageWallets) {
     let newWalletInfoItem = document.createElement('wallet-info');
     newWalletInfoItem.data = wallet;
-    console.log(newWalletInfoItem);
     walletGrid.appendChild(newWalletInfoItem);
 }
