3dfcde2612fee97055eedad6f7451b1c5525fb6d
jacobgraven <jacobmgraven@gmail.com>
2022-11-14 17:37:20 -0800

    Fixing report styles (final changes)

--- source/javascript/Report.js
+++ source/javascript/Report.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', () => {
-    // TODO: Get yearly spending value
-    let yearlySpending = 0;
-    document.getElementById('yearly-spending').innerHTML = "Yearly Spending: $" + yearlySpending;
+  // TODO: Get yearly spending value
+  let yearlySpending = 0;
+  document.getElementById('yearly-spending').innerHTML = "Yearly Spending: $" + yearlySpending;
 
 });
 
 window.addEventListener('load', () => {
-    let xLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    // Sample Values
-    let yValues = [2500, 3500, 3000, 2100, 1000, 1500, 1250, 500, 750, 900, 1000, 1500];
+  let xLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  // Sample Values - TODO: loop over transactions from each month, assign to yValues
+  let yValues = [2500, 3500, 3000, 2100, 1000, 1500, 1250, 500, 750, 900, 1000, 1500];
 
-new Chart("report-chart", {
+  new Chart("report-chart", {
     type: "bar",
     data: {
       labels: xLabels,
@@ -18,5 +18,13 @@ new Chart("report-chart", {
         data: yValues
       }]
     },
+    options: {
+      legend: {
+        display: false
+      },
+      tooltips: {
+        enabled: false
+      }
+    }
   });
 });
