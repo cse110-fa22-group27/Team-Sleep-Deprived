2cdf510d1819352a10150bcb091aaf5a3e783a11
Ashwin Rohit Alagiri Rajan <marble@Ashwins-MacBook-Pro.local>
2022-11-26 10:27:42 -0800

    Complete Get Wallets for User

--- source/javascript/WalletsGlobal.js
+++ source/javascript/WalletsGlobal.js
@@ -1,14 +1,14 @@
 import { getAllUsersObject, setAllUsersObject } from './database.js';
 
-const getAllWalletsForUser = (username) => {
-	const allUsersObject = getAllUsersObject();
+const getAllWalletsForUser = async (username) => {
+	const allUsersObject = await getAllUsersObject();
 	const userObject = allUsersObject[username];
 	const walletsObject = userObject.wallets;
 	return walletsObject;
 }
 
-const setWalletsForUser = (username, walletsObject) => {
-	const allUsersObject = getAllUsersObject();
+const setWalletsForUser = async (username, walletsObject) => {
+	const allUsersObject = await getAllUsersObject();
 	const userObject = allUsersObject[username];
 	userObject.wallets = walletsObject;
 	setAllUsersObject(allUsersObject);
