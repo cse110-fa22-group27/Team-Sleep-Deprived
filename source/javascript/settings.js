36fb23393afbf8c7902db18b924aa685e0fd40ad
Oooleaf <114543429+Oooleaf@users.noreply.github.com>
2022-11-27 19:51:36 -0800

    Update settings.js

--- source/javascript/settings.js
+++ source/javascript/settings.js
@@ -1,14 +1,26 @@
-export const storagePage = {};//create wrapper object to hold storage
-let changePassword = document.querySelector('.password-change-setting glass-box setting-component');
+import { getCurrentUser, updateCurrentUser } from "./globals.js";
+const pages = ['../html/dashboard.html', '../html/wallets.html', '../html/report.html'];
+
+const currentUser = await getCurrentUser();
+
+function loadDefaultPage(defaultPageNumber) {
+	window.location.replace(pages[defaultPageNumber]);
+}
+const pageNumber =  currentUser['preferred-default-page'];
+loadDefaultPage(pageNumber);
+
+//change password
+const changePassword = document.querySelector('.password-change-setting glass-box setting-component');
 changePassword.onclick = function(){
     let formOld = new FormData(this);
     let oldpassword = formOld.get('old-password');
     let formNew = new FormData(this);
     let newpassword = formNew.get('new-password');
-    //change password
+
     if(oldpassword != "" && newpassword != ""){
         if(oldpassword != newpassword){
-            user.password = newpassword;
+            currentUser.password = newpassword;
+            
         }
         else{
             alert("Old password and Newpassword cannot be the same!");
@@ -19,23 +31,28 @@ changePassword.onclick = function(){
     }
 }
 
-let choseDefault = document.querySelector('.landing-page-setting glass-box setting-component');
+//change default page
+const choseDefault = document.querySelector('.landing-page-setting glass-box setting-component');
 choseDefault.onclick = function(){
     let input = document.getElementById('page');
     let dashboard = document.getElementById('page').value = 'dashboard';
     let wallet = document.getElementById('page').value ='wallets';
     let report = document.getElementById('page').value ='reports';
-    storagePage = JSON.parse(user.getItem('preferred-default-page'))//get object from db
+
     if(input == dashboard){
-        storagePage = 0;
+        loadDefaultPage(0);
     }
     else if(input == wallet){
-        storagePage = 1;
+        loadDefaultPage(1);
     }
     else if(input == report){
-        storagePage = 2;
+        loadDefaultPage(2);
     }
-    //update current user to all user-object
     
-    //store all user in database
+}
+
+/*update current user to all user-object
+store new user password in database*/
+function updateDB(){
+	updateCurrentUser(currentUser);
 }
