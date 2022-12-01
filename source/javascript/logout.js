e32f90e47723c99337480af96a32dec98bae411b
Andrew Nguyen <3ndrew123@gmail.com>
2022-11-30 18:09:45 -0800

    Changed ref to login.html for logout

--- source/javascript/logout.js
+++ source/javascript/logout.js
@@ -2,7 +2,7 @@ import { resetGlobalInfo } from './globals.js';
 
 function logout() {
 	resetGlobalInfo();
-	window.location.replace('../html/signin.html');
+	window.location.replace('../html/login.html');
 }
 
 const logoutButton = document.querySelector('.logout-button');
