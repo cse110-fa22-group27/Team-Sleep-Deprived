22117923a470ba3d8042208e8c3df8890919506d
red8823 <andy.lu8823@gmail.com>
2022-12-03 07:13:51 -0800

    Update snapshots

--- source/javascript/testing/login.test.js
+++ source/javascript/testing/login.test.js
@@ -1,14 +1,12 @@
 
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
-const { getCurrentUser, setCurrentUsername, getCurrentUsername, resetGlobalInfo } = require ('../globals.js');
-
 beforeAll(async () => {
 	const page = await browser.newPage();
 	await page.goto('http://localhost:5500/source/html/login.html');
 });
 
-describe('Sign-in snapshot testing', () => {
-	it('Testing for any changes to the sign-in page', async () => {
+describe('Log-in snapshot testing', () => {
+	it('Testing for any changes to the log-in page', async () => {
 		const page = await browser.newPage();
 		expect.extend({ toMatchImageSnapshot });
 		await page.setViewport({ width: 1280, height: 1400 });
